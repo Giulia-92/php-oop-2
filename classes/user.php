cbf40a181fbc9aa1b915af673eae58830e6e74e4
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-06-07 16:17:08 +0200

    php-oop-2

---
+++ classes/user.php
@@ -0,0 +1,26 @@
+//creazione di un a classe con delle proprietà
+<?php
+class User {
+protected $nome;
+protected $cognome;
+protected $numero_carta;
+
+//creare una funzione che compie un'azione con all'interno dei valori
+    function __construct($_nome, $_cognome, $_numero_carta) {
+        $this->nome = $_nome;
+        $this->cognome = $_cognome;
+        $this ->numero_carta;
+    }
+public function getNome(){
+    return $this-> nome;
+}
+public function getCognome(){
+    return $this-> cognome;
+}
+public function getNumero_carta(){
+    return $this-> numero_carta;
+}
+
+   
+}
+?>
